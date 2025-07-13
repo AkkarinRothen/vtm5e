b940e1875f19d1c455c1d546408957c9d6486b07
AkkarinRothen <yacris245@gmail.com>
2025-07-13 15:30:51 -0300

    Refactor disciplines data to JSON

--- loadDisciplines.js
+++ loadDisciplines.js
@@ -1,12 +1,8 @@
 const fs = require('fs');
-const vm = require('vm');
 
 function loadDisciplines() {
-  const html = fs.readFileSync('index.html', 'utf8');
-  const match = html.match(/const disciplines = ({[\s\S]*?});/);
-  if (!match) throw new Error('Disciplines object not found');
-  const objectString = match[1];
-  return vm.runInNewContext(`(${objectString})`);
+  const data = fs.readFileSync('public/data/disciplines.json', 'utf8');
+  return JSON.parse(data);
 }
 
 module.exports = loadDisciplines;
